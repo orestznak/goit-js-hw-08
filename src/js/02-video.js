306fdcb6a2afcf4a69b9649743b07685e74d904b
orestznak <51721977+orestznak@users.noreply.github.com>
2022-06-14 17:00:47 +0300

    player
    не працює currentTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGY_KEY = 'videoplayer-current-time';
+
+const videoVimeo = document.querySelector('#vimeo-player');
+
+const player = new Player(videoVimeo);
+
+console.log(player);
+
+
+const onPlay = function() {
+    player.on('timeupdate', function(){
+        console.log('the time was updated to: ' + player.duration);
+    })
+
+    // const currentTimePlayer = player.
+
+    // localStorage.setItem(STORAGY_KEY, currentTimePlayer);
+
+    // player.setCurrentTime(localStorage.getItem(STORAGY_KEY))
+    // 
+};
+
+player.on('play', onPlay);
+
+
+// throttle(onPlay,250)
