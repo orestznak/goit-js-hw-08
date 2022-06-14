db094156ce9cf625fad7ee5d332187ba3fe855eb
orestznak <51721977+orestznak@users.noreply.github.com>
2022-06-14 09:40:53 +0300

    Завдання 1 -
    Додав бібліотеку SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css"
 
 console.log(galleryItems);
+const imgGallery = document.querySelector('.gallery');
+
+const listImages = galleryItems
+                    .map(item => `<div class='gallery__item'>
+                                    <a class="gallery__link" href=${item.original}>
+                                        <img class='gallery__image'
+                                            src = ${item.preview}
+                                            alt = ${item.description}
+                                        />
+                                    </a>
+                                </div>`)
+                    .join('');
+//console.log(listImages)
+
+imgGallery.innerHTML = listImages;
+
+imgGallery.addEventListener('click', showModal);
+
+const lightbox = new SimpleLightbox('.gallery a', {captions: true, captionSelector: 'img', captionsData: 'alt', captionDelay: 250});
+
+function showModal(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    console.log(event.target)
+    event.preventDefault();
+}
