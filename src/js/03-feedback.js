707e74d44b3410c2df0fe3963af939b96cee97b7
orestznak <orestznak@gmail.com>
2022-07-03 19:25:35 +0300

    update 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
+
+import '../css/03-feedback.css'
+
+const STORAGE_KEY = "feedback-form-state";
+let formData = {};
+
+const feedBackForm = document.querySelector('form');
+feedBackForm.addEventListener('input',throttle(onInput,500))
+feedBackForm.addEventListener('submit',submitFeedBack)
+
+populateData();
+
+function onInput(evt) {
+    evt.preventDefault();
+    formData [evt.target.name] = evt.target.value;
+    console.log(formData);
+    
+    localStorage.setItem(STORAGE_KEY,JSON.stringify(formData));
+
+}
+
+function populateData() {
+    const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if(saveData) {
+        feedBackForm.email.value = saveData.email;
+        feedBackForm.message.value = saveData.message;
+    }
+}
+
+function submitFeedBack(evt) {
+    evt.preventDefault();
+
+    feedBackForm.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
 
